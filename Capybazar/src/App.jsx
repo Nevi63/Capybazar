191fc2805a9c7ace8d9493612fcaf0824fba9028
Hamoncit0 <151789307+Hamoncit0@users.noreply.github.com>
2025-03-17 14:42:16 -0600

    Home, Routes

--- Capybazar/src/App.jsx
+++ Capybazar/src/App.jsx
@@ -1,24 +1,78 @@
-import './App.css'
-import { Routes, Route } from "react-router-dom";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/home';
-import About from './pages/About/about';
 import Login from './pages/Login/login';
 import Signup from './pages/Signup/signup';
-import { Typography, Container } from "@mui/material";
-
+import Cart from './pages/Cart/Cart';
+import WishList from './pages/WishList/WishList';
+import PurchaseHistory from './pages/PurchaseHistory/PurchaseHistory';
+import EditProfile from './pages/EditProfile/EditProfile';
+import AdvancedSearch from './pages/AdvancedSearch/advancedSearch';
+import CheckOut from './pages/CheckOut/CheckOut';
+import ProductInformationClient from './pages/ProductInformationClient/ProductInformationClient';
+import CreateProduct from './pages/CreateProduct/CreateProduct';
+import Inventory from './pages/Inventory/Inventory';
+import ProductInformation from './pages/ProductInformation/ProductInformation';
+import ProductList from './pages/ProductList/ProductList';
+import CategoryList from './pages/CategoryList/CategoryList';
+import ProductInformationAdmin from './pages/ProductInformationAdmin/ProductInformationAdmin';
+import ProductListAdmin from './pages/ProductListAdmin/ProductListAdmin';
+import { Typography } from '@mui/material';
+import { GuestRoute, GuestClientRoute, ClientRoute, SellerRoute, AdminRoute, AuthenticatedRoute } from './RouteGuards'; // Importa tus guardias
+import Navbar from './components/navbar/navbar';
 function App() {
-
+  const user = 'admin' 
+  // cliente, guest, vendedor, admin
   return (
     <>
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Typography variant="h4">Página no encontrada</Typography>} />
+      {user && <Navbar userType={user}></Navbar>}
+      <Routes>
+        {/* guest & client*/}
+        <Route  element={<GuestClientRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/advancedSearch" element={<AdvancedSearch />} />
+          <Route path="/productInfo" element={<ProductInformationClient />} />
+        </Route>
+
+        {/* guest */}
+        <Route element={<GuestRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+
+        {/* anyone logged in */}
+        <Route element={<AuthenticatedRoute />}>
+            <Route path="/editProfile" element={<EditProfile />} />
+        </Route>
+
+        {/* client */}
+        <Route element={<ClientRoute />}>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/purchaseHistory" element={<PurchaseHistory />} />
+          <Route path="/checkOut" element={<CheckOut />} />
+        </Route>
+
+        {/* seller */}
+        <Route element={<SellerRoute />}>
+          <Route path="/createProduct" element={<CreateProduct />} />
+          <Route path="/editProduct" element={<CreateProduct />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/productInformation" element={<ProductInformation />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Route>
+
+        {/* admin */}
+        <Route element={<AdminRoute />}>
+          <Route path="/categoryList" element={<CategoryList />} />
+          <Route path="/productListAdmin" element={<ProductListAdmin />} />
+          <Route path="/reviewManagement" element={<ProductInformationAdmin />} />
+        </Route>
+
+        <Route path="*" element={<Typography sx={{ p: 5 }} variant="h4">Página no encontrada</Typography>} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Capybazar/src/components/carousel/carousel.jsx
+++ Capybazar/src/components/carousel/carousel.jsx
@@ -2,13 +2,50 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const images = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScUD4YEbsDybK3wILoeKezLB-ANesskAAswQ&s",
   "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000083903/d551b2dee1b267f36000ce3ae52b20001cb26dc0fa0609a89defd7896e38ab66",
   "https://img.rtve.es/imagenes/hatsune-miku-artista-pop-digital-cazador-cerebros/1689160036357.jpg",
 ];
+// Botón personalizado para la izquierda
+const PrevArrow = ({ onClick }) => (
+  <IconButton
+    onClick={onClick}
+    sx={{
+      zIndex: 2,
+      position: "absolute",
+      left: 10,
+      top: "50%",
+      transform: "translateY(-50%)",
+      backgroundColor: "rgba(0,0,0,0.5)",
+      color: "white",
+      "&:hover": { backgroundColor: "rgba(0,0,0,0.8)" },
+    }}
+  >
+    <ArrowBackIos />
+  </IconButton>
+);
+
+// Botón personalizado para la derecha
+const NextArrow = ({ onClick }) => (
+  <IconButton
+    onClick={onClick}
+    sx={{
+      position: "absolute",
+      right: 10,
+      top: "50%",
+      transform: "translateY(-50%)",
+      backgroundColor: "rgba(0,0,0,0.5)",
+      color: "white",
+      "&:hover": { backgroundColor: "rgba(0,0,0,0.8)" },
+    }}
+  >
+    <ArrowForwardIos />
+  </IconButton>
+);
 
 const ImageCarousel = () => {
   const settings = {
@@ -19,13 +56,16 @@ const ImageCarousel = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    nextArrow: <NextArrow />, // Personaliza el botón derecho
+    prevArrow: <PrevArrow />, // Personaliza el botón izquierdo
   };
 
   return (
-    <Box sx={{ width: "100%", margin: "auto", mt: 4 }}>
+    <Box sx={{ width: "100%", maxHeight: "500px", mb: 2 }}>
       <Slider {...settings}>
         {images.map((img, index) => (
-          <Box key={index} component="img" src={img} alt={`slide-${index}`} sx={{ width: "100%", borderRadius: 2 }} />
+          <Box key={index} component="img" src={img} alt={`slide-${index}`} 
+               sx={{ width: "100%", maxHeight: "500px", borderRadius: 2, objectFit: "fill" }} />
         ))}
       </Slider>
     </Box>
